05588d5ad984056f204c08ca9a3cdd3ed402015d
Aziz Haknazarov <haknazarov.aziz@mail.ru>
2020-10-17 18:11:08 +0100

    Added comments to code

--- main.go
+++ main.go
@@ -14,11 +14,19 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
+// our new stats function which will expose any YouTube
+// stats via a websocket connection
 func stats(w http.ResponseWriter, r *http.Request) {
+	// we call our new websocket package Upgrade
+    // function in order to upgrade the connection
+    // from a standard HTTP connection to a websocket one
 	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
 		fmt.Fprintf(w, "%+v\n", err)
 	}
+	// we then call our Writer function
+    // which continually polls and writes the results
+    // to this websocket connection
 	go websocket.Writer(ws)
 }
 
